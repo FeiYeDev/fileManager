3ca76b17ff1f5c248d83dfc75897384d03e99541
yf-0326 <fye@fintechautomation.com>
2022-11-29 03:00:52 +0800

    fix

--- src/generateTree.js
+++ src/generateTree.js
@@ -1,18 +1,48 @@
 export default function generateTree() {
   let tree = {
-    selected: {
-      id: 0,
-      filePaths: ['http:xxxxxxxx.png'],
-      folderName: 'Files',
-      childIds: [],
-      parentIds: []
-    },
     0: {
       id: 0,
-      filePaths: ['http:xxxxxxxx.png'],
+      filePaths: [
+        'http:xxxxxxxx.png'
+      ],
       folderName: 'Files',
-      childIds: [],
+      childIds: [
+        'new_0',
+        'new_1'
+      ],
       parentIds: []
+    },
+    selected: {
+      id: 0,
+      filePaths: [
+        'http:xxxxxxxx.png'
+      ],
+      folderName: 'Files',
+      childIds: [
+        'new_0',
+        'new_1'
+      ],
+      parentIds: []
+    },
+    new_0: {
+      id: 'new_0',
+      folderName: 'Documents',
+      parentIds: [
+        0
+      ],
+      filePaths: [],
+      childIds: []
+    },
+    new_1: {
+      id: 'new_1',
+      folderName: 'Music',
+      parentIds: [
+        0
+      ],
+      filePaths: [
+        'xxxx.mp3'
+      ],
+      childIds: []
     }
   }
 
