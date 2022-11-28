111f724a10e4d37d6f4679770a6b0c86e1f99af1
yf-0326 <fye@fintechautomation.com>
2022-11-29 01:54:21 +0800

    push

--- src/generateTree.js
+++ src/generateTree.js
@@ -11,8 +11,29 @@ export default function generateTree() {
       id: 0,
       filePaths: ['http:xxxxxxxx.png'],
       folderName: 'Files',
-      childIds: [],
+      childIds: [1,2],
       parentIds: []
+    },
+    1: {
+      id: 1,
+      filePaths: ['http:ssssssss.png'],
+      folderName: 'Documents',
+      childIds: [],
+      parentIds: [0]
+    },
+    2: {
+      id: 2,
+      filePaths: ['http:fffffffff.png'],
+      folderName: 'Music',
+      childIds: [3],
+      parentIds: [0]
+    },
+    3: {
+      id: 3,
+      filePaths: [],
+      folderName: 'R&B',
+      childIds: [],
+      parentIds: [2]
     }
   }
 
